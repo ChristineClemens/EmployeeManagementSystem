78c9d6c6ae6c144a5220ebc280bfbf5a69df7f97
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-30 15:05:23 -0400

    Added a .gitignore and .env file, downloaded node modules, initialized server.js setup.

---
+++ server.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+require("console.table");
+
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            });
+        });
+    }
+}
+
+// Access SQL Database
+const db = new Database({
+    host: "localhost",
+    port: 1080,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    insecureAuth : true
+});
