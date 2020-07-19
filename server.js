8ab1b64a47514f4bfb9764832b934624d31f6fc2
Christine Clemens <christineclemens.tesol@gmail.com>
2020-07-19 19:11:05 -0400

    Added mainMenu and employeeMenu prompts.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-require("console.table");
 
+const consoleTable = require("console.table");
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
@@ -39,8 +39,102 @@ const db = new Database({
 });
 
 async function mainApp() {
-    let employeeList = await db.query("SELECT * FROM cms_db.employee");
-    console.log(employeeList);
+    const mainMenu = await inquirer.prompt ([
+        {
+            message: "What would you like to do?",
+            name: "mainMenu",
+            type: "list",
+            choices: [
+                {
+                    name: "Manage Employees",
+                    value: "employees"
+                },
+                {
+                    name: "Manage Roles",
+                    value: "roles"
+                },
+                {
+                    name: "Manage Departments",
+                    value: "departments"
+                },
+                {
+                    name: "Exit",
+                    value: "exit"
+                }
+            ]
+        }
+    ]) .then ((selection) => selection.mainMenu);
+
+    switch (selection) {
+        case "employees":
+        var employeesMenu = await inquirer.prompt ([
+            {
+                message: "Please select an option below:",
+                name: "employeesMenu",
+                type: "list",
+                choices: [
+                    {
+                        name: "View existing employees",
+                        value: "view"
+                    },
+                    {
+                        name: "Add a new employee",
+                        value: "add"
+                    },
+                    {
+                        name: "Update an existing employee's role",
+                        value: "update"
+                    },
+                    {
+                        name: "Update an existing employee's manager",
+                        value: "updateManager"
+                    },
+                    {
+                        name: "Remove an existing employee",
+                        value: "remove"
+                    }
+                ]
+            }
+        ]) .then ((response) => response.employeesMenu)
+    }
+
+
 }
 
 mainApp();
+
+
+
+
+
+
+
+
+
+async function insertOne(tableName, value) {
+    return new Promise((resolve, reject) => {
+        this.connection.query("INSERT INTO ?? SET ?", [tableName, value], function (err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+}
+
+async function updateOne(tableName, update, condition) {
+    //requires a string for tablename, and objects with a single key value pair for update and condition
+    return new Promise((resolve, reject) => {
+        this.connection.query("UPDATE ?? SET ? WHERE ?", [tableName, update, condition], function (err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+}
+
+async function removeOne(tableName, condition) {
+    return new Promise((resolve, reject) => {
+        this.connection.query("DELETE FROM ?? WHERE ?", [tableName, condition], function (err, rows) {
+            if (err) reject(err);
+            resolve(rows);
+        });
+    });
+}
